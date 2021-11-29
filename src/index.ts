f858629472e61baa4778eb2e977e18034160fbd5
Jordan Wilkinson <82219425+Zymmlol@users.noreply.github.com>
2021-11-29 12:27:41 +0000

    puppeteer yoi (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,122 @@
 // Import the puppeteer package
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
-// Create an "async" environment so we can use "await"
-;(async () => {
+  // Create an "async" environment so we can use "await"
+  ; (async () => {
 
-  // Create a virtual browser using puppeteer - this is the equivalent of opening up Chrome on your PC
-  const browser = await puppeteer.launch({
-    headless: true,
-    defaultViewport: {
-      width: 1920,
-      height: 1080
-    }
-  })
+    // Create a virtual browser using puppeteer - this is the equivalent of opening up Chrome on your PC
+    puppeteer.use(StealthPlugin())
+    const browser = await puppeteer.launch({
+      headless: true,
+      ignoreHTTPSErrors: true,
+      defaultViewport: {
+        width: 1920,
+        height: 1080
+      }
+    })
 
-  // Create a virtual "page" - this is the equivalent of opening a new tab in your Chrome browser
-  const page = await browser.newPage()
+    // Create a virtual "page" - this is the equivalent of opening a new tab in your Chrome browser
+    const page = await browser.newPage()
+    page.on('console', message => console.log(`[PAGE CONSOLE] ${message.text()}`))
 
-  // IFRAME LINK: https://www.plus.net
-  await page.goto('https://www.waterstones.com')
+    // IFRAME LINK: https://www.plus.net
+    await page.goto('https://www.plus.net')
+    const keywords = [
+      'accept',
+      'agree',
+      'allow',
+    ]
 
-  // get iframe
-  // const iframe = await page.$('iframe[src*="trustarc.com"]')
-  // if (iframe) {
-  //   const frame = await iframe.contentFrame()
-  //   if (frame) {
-  //     await frame.evaluate(() => {
-  //       // FIND BUTTON WITH SAME LOGIC
-  //     })
-  //   }
-  // }
+    await page.waitForTimeout(5000)
 
-  await page.waitForTimeout(2000)
+    // Take a screenshot so we can see what Puppeteer "sees"
+    await page.screenshot({
+      path: './screenshots/pre_cookies3.png'
+    })
 
-  // Take a screenshot so we can see what Puppeteer "sees"
-  await page.screenshot({
-    path: './screenshots/pre_cookies.png'
-  })
+    // get iframe
+    const iframe = await page.$('iframe[src*="trustarc.com"]')
+    if (iframe) {
+      const frame = await iframe.contentFrame()
+      if (frame) {
+        const result = await frame.evaluate((keywords: string[]) => {
+          // Any code inside of here is executed on the PAGE - we cannot mix and match variables inside here with our Node project outside
 
-  // We can use the "evaluate" method to execute JavaScript on the website - this is the equivalent of typing JS into the Chrome console
-  const keywords = [
-    'accept',
-    'cookies',
-    'accept all',
-    'ok',
-    'okay'
-  ]
-  
-  const result = await page.evaluate((keywords) => {
-    // Any code inside of here is executed on the PAGE - we cannot mix and match variables inside here with our Node project outside
+          const allElements = document.querySelectorAll('*')
 
-    const allElements = document.querySelectorAll('*')
+          for (let i = 0; i < allElements.length; i++) {
+            const element: any = allElements[i]
+            if (element && element.innerText) {
+              // if (element.className === "base-components__BaseElement-sc-1mosoyj-0 styled__TextSpan-sc-3mnirm-4 gzHQpg beans-button__text")
+              // console.log(element.innerText)
+              const elementContainsKeyword = keywords.some(s => element.innerText.toLowerCase().includes(s))
+              const elementHasOK = element.innerText.toLowerCase() === "ok"
+              const elementArray = ['A', 'SPAN', 'BUTTON']
+              const elementIsButton = elementArray.includes(element.tagName)
+              if ((elementContainsKeyword || elementHasOK) && elementIsButton) {
+                element.click()
+                console.log(element.innerText)
+                return true
+              }
+            }
+          }
 
-    for (let i = 0; i < allElements.length; i++) {
-      const element: any = allElements[i]
-      if (keywords.includes(element.innerText.toLowerCase())) {
-        element.click()
-        return true
+
+          return false
+        }, keywords)
+        console.log(`Found button: ${result}`)
+        // Let the user know we're done
+        console.log('Done!')
       }
     }
+    await page.waitForTimeout(3000)
+    await page.screenshot({
+      path: './screenshots/post_cookies3.png'
+    })
 
-    return false
-  }, keywords)
 
-  console.log(`Found button: ${result}`)
+    // We can use the "evaluate" method to execute JavaScript on the website - this is the equivalent of typing JS into the Chrome console
+    // const keywords = [
+    //   'accept',
+    //   'agree',
+    //   'allow',
+    // ]
 
-  await page.waitForTimeout(2000)
+    // const result = await page.evaluate((keywords: string[]) => {
+    //   // Any code inside of here is executed on the PAGE - we cannot mix and match variables inside here with our Node project outside
 
-  await page.screenshot({
-    path: './screenshots/post_cookies.png'
-  })
+    //   const allElements = document.querySelectorAll('*')
 
-  // Let the user know we're done
-  console.log('Done!')
-})()
+    //   for (let i = 0; i < allElements.length; i++) {
+    //     const element: any = allElements[i]
+    //     if (element && element.innerText){
+    //       // if (element.className === "base-components__BaseElement-sc-1mosoyj-0 styled__TextSpan-sc-3mnirm-4 gzHQpg beans-button__text")
+    //       // console.log(element.innerText)
+    //       const elementContainsKeyword = keywords.some(s => element.innerText.toLowerCase().includes(s))
+    //       const elementHasOK = element.innerText.toLowerCase() === "ok"
+    //       const elementIsButton = element.tagName === 'BUTTON'
+    //       const elementArray = ['a', 'span', '']
+    //       if ((elementContainsKeyword || elementHasOK) && elementIsButton) {
+    //         element.click()
+    //         console.log(element.tagName)
+    //         return true
+    //       }
+    //     }
+    //   }
+
+
+    //   return false
+    // }, keywords)
+
+    // console.log(`Found button: ${result}`)
+
+    // await page.waitForTimeout(3000)
+
+    // await page.screenshot({
+    //   path: './screenshots/post_cookies3.png'
+    // })
+
+    // // Let the user know we're done
+    // console.log('Done!')
+  })()

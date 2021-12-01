5c62835f4981c049e841f53f3ad0f533dffaf2f0
Jordan Wilkinson <82219425+Zymmlol@users.noreply.github.com>
2021-12-01 11:27:00 +0000

    WIP (#2)
    * puppeteer yoi
    
    * WIP
    
    Co-authored-by: Jordan Wilkinson <Jordan.Wilkinson@moonpullpartners.com>

--- src/findAndClickButton.ts
+++ src/findAndClickButton.ts
@@ -3,22 +3,18 @@ export default function findAndClickButton(keywords: string[]) {
   // Any code inside of here is executed on the PAGE - we cannot mix and match variables inside here with our Node project outside
 
   const allElements = document.querySelectorAll('*')
-
   for (let i = 0; i < allElements.length; i++) {
-
     const element: any = allElements[i]
+    if (element.id === "onetrust-accept-btn-handler") {
+      console.log(element.innerText)
+   }
     if (element && element.innerText) {
-      
-      const elementContainsKeyword = keywords.some(s => element.innerText.toLowerCase().includes(s))
-      const elementHasOK = element.innerText.toLowerCase() === "ok"
-      const elementArray = ['A', 'SPAN', 'BUTTON']
-      const elementIsButton = elementArray.includes(element.tagName)
-      if ((elementContainsKeyword || elementHasOK) && elementIsButton) {
+      const elementContainsKeyword = keywords.some(word => element.innerText.toLowerCase().trim() === word)
+      if ((elementContainsKeyword)) {
         element.click()
-        console.log(element.innerText)
         return true
       }
-    }
+    } 
+    return false
   }
-  return false
 }
